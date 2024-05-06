897dbd801a082d148b130f3aa8956125c8de0003
enginedilema <omiro@xtec.cat>
2024-05-06 21:05:13 +0200

    Add firebase/php-jwt and guzzlehttp/guzzle dependencies

--- app/Http/Controllers/ApiNewsController.php
+++ app/Http/Controllers/ApiNewsController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Article;
+use GuzzleHttp\Client;
+use Firebase\JWT\JWT;
 
 class ApiNewsController extends Controller
 {
@@ -45,6 +47,77 @@ class ApiNewsController extends Controller
         $article->article_title = $request->input('title');
         $article->article_description = $request->input('content');
         $article->save();
-        return var_dump($article->title);
+        $this->publica($article);
+        return $this->getJWT();
+    }
+    public function publica($article){
+        $client = new Client();
+
+        $base_uri = env("WP_BASE_URL").'/jwt-auth/v1/token';
+
+        $response = $client->request('POST', $base_uri, [
+            'headers' => [
+            ],
+            'json' => [
+                'username' => env('WP_USER'),
+                'password' => env('WP_PASS'),
+            ]
+        ]);
+        if ($response->getStatusCode() == 200) {
+            $response = json_decode($response->getBody());
+            $token = $response->token;
+            $client = new Client();
+            $base_uri = env("WP_BASE_URL").'/wp/v2/posts';
+            $auth = 'Bearer ' . $token;
+    
+            try {
+            $response = $client->request('POST', $base_uri , [
+                'headers' => [
+                    'Authorization' => $auth,
+                    'Content-Type' => 'application/json'
+                ],
+                'json' => [
+                    'title' => $article->article_title,
+                    'content' => $article->article_description,
+                    'status' => 'publish'
+                ]
+            ]);
+            if ($response->getStatusCode() == 201) {
+                $article->is_published = true;
+                $article->save();
+                // Podrías retornar algo aquí, por ejemplo, un mensaje de éxito o la respuesta.
+            } else {
+                // Manejar situaciones donde la API no devuelve un estado 201 (creado)
+                return "Error al publicar el artículo, código de estado: " . $response->getStatusCode();
+            }
+        } catch (RequestException $e) {
+            // Manejar excepciones de la solicitud, como errores de conexión o HTTP
+            return "Error al publicar el artículo: " . $e->getMessage();
+        }
+            // Podrías retornar algo aquí, por ejemplo, un mensaje de éxito o la respuesta.
+        } else {
+            // Manejar situaciones donde la API no devuelve un estado 201 (creado)
+            return "Error al publicar el artículo, código de estado: " . $response->getStatusCode();
+        }
+
+       
+    
+    }
+
+    function getJWT() {
+        $key = env("JWT_KEY");  // La clave secreta, debe ser la misma en wp-config.php
+        $issuedAt = time();  // Tiempo actual en segundos desde la época Unix
+        $expirationTime = $issuedAt + 3600;  // Token expira en 1 hora
+        $serverName = 'https://oriolmiro.com';  // Asegúrate de que corresponda al dominio de tu sitio de WordPress
+    
+        $payload = [
+            'iss' => $serverName,  // Emisor
+            'iat' => $issuedAt,  // Tiempo de emisión
+            'exp' => $expirationTime,  // Tiempo de expiración
+            'user_id' => 2,  // ID del usuario de WordPress
+        ];
+        $token = JWT::encode($payload, $key, 'HS256');  // Codifica y retorna el token
+        print_r($token);
+        return $token;  // Codifica y retorna el token
     }
 }
